a56d3af092681cb7db05e3715724101cb562209d
s31175 <s31175@pjwstk.edu.pl>
2025-01-25 13:08:35 +0100

    test

---
+++ s31175Bank/src/main/java/com/example/s31175Bank/S31175BankApplication.java
@@ -0,0 +1,13 @@
+package com.example.s31175Bank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S31175BankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S31175BankApplication.class, args);
+	}
+
+}
---
+++ s31175Bank/src/test/java/com/example/s31175Bank/S31175BankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.s31175Bank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S31175BankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
